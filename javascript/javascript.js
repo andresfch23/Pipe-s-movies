08e0474758daaebdd7ce1e0a25260727f727473a
Andres <andresfch23@gmail.com>
2017-10-26 18:17:07 -0500

    agregue dinamicamente las opciones del select

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,40 @@
 $(document).ready(function(){
-    $(".navbar__button").click(function(){
-        $(".main__left-menu").toggle();
+    var id = 'c0b2e256491361d28c75bbe8f9e59a85';
+    var baseUrl = 'https://api.themoviedb.org/3/';
+    var getGenresUrl = `${baseUrl}genre/movie/list?api_key=${id}&language=en-US`;
+    var $navbarButton = $('.navbar__button');
+    var $leftMenu = $(".main__left-menu");
+    var $leftSelect = $(".main__left-select");
+    
+
+    var onToggleMenu = () => $leftMenu.toggle();
+
+    var onRenderSelectOptions = (results) => {
+        results.genres.forEach((genre) => {
+            $leftSelect.append(`<option value=${genre.id}>${genre.name}</option>`);
+        });
+    }
+
+    // Function to render select options
+    var onGetGenres = (results) => {
+        onRenderSelectOptions(results);
+    };
+    
+    // Function to toggle left menu
+    $navbarButton.click(onToggleMenu);
+
+    // Function to get movie genres and 
+    $.get(getGenresUrl, onGetGenres);
+
+    $leftSelect.change(() => {
+        var selectedGenreId = $(this).find("option:selected").val();    
+        var getMoviesGenre = `${baseUrl}genre/${selectedGenreId}/movies?api_key=${id}&language=en-US&include_adult=false&sort_by=created_at.asc`;
+        
+        $.get(getMoviesGenre, (response) => {
+            console.log(response);
+        });
     });
+    
+    
+    
 });
